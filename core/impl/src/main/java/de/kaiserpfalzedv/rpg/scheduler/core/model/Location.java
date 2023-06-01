c4d1b64867b810e434bc118b5c8d22e0f6b2daac
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2023-06-01 13:31:52 +0200

    WIP: transfer to other workstation.

--- core/impl/src/main/java/de/kaiserpfalzedv/rpg/scheduler/core/model/Location.java
+++ core/impl/src/main/java/de/kaiserpfalzedv/rpg/scheduler/core/model/Location.java
@@ -1,15 +1,32 @@
 package de.kaiserpfalzedv.rpg.scheduler.core.model;
 
+import java.time.Duration;
 import java.util.UUID;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.Guild;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
-import lombok.Builder.Default;
-import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import lombok.extern.jackson.Jacksonized;
 
+@Entity
+@Table(
+    name = "LOCATIONS", 
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"serverName", "channelName"})
+    }
+)
 @Jacksonized
 @Builder(toBuilder = true)
 @Getter
@@ -17,7 +34,16 @@ import lombok.extern.jackson.Jacksonized;
 @EqualsAndHashCode(of = "id")
 @ToString(of = {"id"})
 public class Location implements de.kaiserpfalzedv.rpg.scheduler.api.Location {
-    @Default
-    private UUID id = UUID.randomUUID();
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    private String serverName;
+    private String channelName;
+
+    public void play() {
+        GatewayDiscordClient client = DiscordClient.create(channelName).gateway().login().block(Duration.ofMinutes(2));
+
+        Guild guild;
+    }
 }
